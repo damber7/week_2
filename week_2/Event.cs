6a4665b70c4b5dd15add2b0fd7d4de9360bf6c54
damber7 <dambert999@gmail.com>
2024-03-03 10:12:47 +0545

    Add project files.

---
+++ week_2/Event.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_2
+{
+    public class TemperatureSensor
+    {
+        private int temperature;
+        public delegate void TemperatureChangedEventHandler(Object sender);
+        public event TemperatureChangedEventHandler TemperatureChanged;
+
+        public int Temperature
+        {
+            get { return temperature; }
+            set
+            {
+                if(value != temperature) {
+                    temperature = value;
+                    OnTemperatureChanged();
+                }
+            }
+        }
+
+        public virtual void OnTemperatureChanged()
+        {
+            if(TemperatureChanged != null)
+            {
+                TemperatureChanged(this);
+            }
+        }
+
+    }
+
+    public class TemperatureMonitor
+    {
+        public void Connect(TemperatureSensor sensor)
+        {
+            sensor.TemperatureChanged += HandleTemperatureChanged;
+        }
+
+        private void HandleTemperatureChanged(object sender)
+        {
+            Console.WriteLine($"Temperature changed. New temperature:" + $"{((TemperatureSensor)sender).Temperature} degree Celsius"); 
+        }
+    }
+
+    class EventDemo
+    {
+        static void Main()
+        {
+            TemperatureSensor sensor = new TemperatureSensor();
+            TemperatureMonitor monitor = new TemperatureMonitor();
+            monitor.Connect(sensor);
+
+            sensor.Temperature = 20;
+            sensor.Temperature = 25;
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+    
+}
---
+++ week_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+class Program
+{
+    static  async Task Main1()
+    {
+        Console.WriteLine("Main Thread started");
+        SyncOperation();
+        Stopwatch sw = Stopwatch.StartNew();
+        AsyncOperation(1);
+        await AsyncOperation(2);
+        sw.Stop();
+        Console.WriteLine($"Both asycn methods completed in {sw.ElapsedMilliseconds} ms.");
+
+        
+    }
+
+    static void SyncOperation()
+    {
+        Console.WriteLine("Synchronous operation started");
+        Stopwatch syncStopWatch = Stopwatch.StartNew();
+        for (int i = 0; i < 5; i++) {
+            Console.WriteLine($"Sync Operation - Step {i + 1}");
+            Thread.Sleep(1000);
+        }
+        syncStopWatch .Stop();
+        Console.WriteLine($"Synchronous methods completed in {syncStopWatch.ElapsedMilliseconds} ms.\n\n");
+
+    }
+
+    static async Task AsyncOperation(int taskNum)
+    {
+        Console.WriteLine($"Asynchronous operation {taskNum} started");
+        Stopwatch asyncStopWatch = Stopwatch.StartNew();
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Async Operation {taskNum} - Step {i + 1}");
+            await Task.Delay(1000);
+        }
+        asyncStopWatch.Stop();
+        Console.WriteLine($"AsynchronousOperation {taskNum} completed in {asyncStopWatch.ElapsedMilliseconds} ms.\n\n");
+
+    }
+}
